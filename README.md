85d8f1a514873e86c78c7cf0264cd78ac1043375
grace myers <gracemyers@graces-mbp.attlocal.net>
2023-12-10 21:41:49 -0600

    finshed

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # PasswordCracker
 A simple password cracking tool with support for MD5, BCrypt, and SHA-256
+
+
+
+This project includes a simple brute-force password cracker using MD5 hashing.
+
+## How to Run
+
+1. Open a terminal or command prompt.
+2. Navigate to the project directory.
+3. Run the script: `python brute_force.py`
+4. Enter the MD5 hash to crack when prompted.
+
--- brute_force.py
+++ brute_force.py
@@ -1,4 +1,8 @@
+import sys
+import bcrypt
+import os
 import hashlib
+
 
 def crack_md5(target_hash, password_list):
     for password in password_list:
@@ -7,13 +11,50 @@ def crack_md5(target_hash, password_list):
             return password
     return None
 
-if __name__ == "__main__":
-    target_hash = input("Enter the MD5 hash to crack: ")
-    password_list = ["password", "123456", "admin", "letmein", "qwerty", "abcdef", "secret"]
+def crack_bcrypt(target_hash, password_list):
+    for password in password_list:
+        hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+        if hashed_password == target_hash:
+            return password
+    return None
 
-    cracked_password = crack_md5(target_hash, password_list)
-    
+def crack_sha256(target_hash, password_list):
+    for password in password_list:
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        if hashed_password == target_hash:
+            return password
+    return None
+
+def main():
+    file_path = "passwords.txt"
+    if not os.path.exists(file_path):
+        print(f"Error: File '{file_path}' not found. Make sure the file exists.")
+        sys.exit(1)
+
+    with open(file_path, "r") as file:
+        password_list = [line.strip() for line in file]
+
+    target_hash = input("Enter the hash to crack: ")
+
+    hash_method = input("Choose password hash method (1: MD5, 2: BCrypt, 3: SHA-256): ")
+
+    if hash_method == "1":
+        crack_method = crack_md5
+    elif hash_method == "2":
+        crack_method = crack_bcrypt
+    elif hash_method == "3":
+        crack_method = crack_sha256
+    else:
+        print("Invalid choice. Exiting.")
+        sys.exit(1)
+
+    cracked_password = crack_method(target_hash, password_list)
+
     if cracked_password:
         print(f"Password cracked: {cracked_password}")
     else:
         print("Password not found in the list.")
+
+if __name__ == "__main__":
+    main()
+
